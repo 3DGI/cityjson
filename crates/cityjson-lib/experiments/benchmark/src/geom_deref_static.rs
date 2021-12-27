c471b1e3a55d52f071119b5fe913b9e0449909e4
Balázs Dukai <balazs.dukai@gmail.com>
2021-12-27 16:59:21 +0100

    Store Semantics on the Surface

--- crates/cityjson-lib/experiments/benchmark/src/geom_deref_static.rs
+++ crates/cityjson-lib/experiments/benchmark/src/geom_deref_static.rs
@@ -2,8 +2,26 @@
 use std::collections::HashMap;
 use std::ops::Mul;
 
-struct SemanticSurface {
-    semtype: String,
+#[derive(Debug)]
+enum SemanticSurface {
+    RoofSurface,
+    GroundSurface,
+    WallSurface,
+}
+
+struct Material {
+    name: String,
+    ambient_intensity: Option<f32>,
+    diffuse_color: Option<[f32; 3]>,
+    emissive_color: Option<[f32; 3]>,
+    specular_color: Option<[f32; 3]>,
+    shininess: Option<f32>,
+    transparency: Option<f32>,
+    is_smooth: Option<bool>,
+}
+
+struct Texture {
+    image: String,
 }
 
 struct Semantics {
@@ -14,30 +32,58 @@ struct Semantics {
 type Vertices = Vec<[f64; 3]>;
 
 type Point = [f64; 3];
-type MultiPoint = Vec<Point>;
-type Ring = Vec<Point>;
-type Surface = Vec<Ring>;
+type LineString = Vec<Point>;
+pub struct Surface {
+    boundaries: Vec<LineString>,
+    semantics: Option<SemanticSurface>,
+    material: Option<Material>,
+    texture: Option<Texture>,
+}
 type Shell = Vec<Surface>;
-type MultiSurface = Vec<Surface>;
-type Solid = Vec<Shell>;
 
-enum Geometry {
-    Point(Point),
-    MultiPoint(MultiPoint),
-    Solid(Solid),
-    MultiSurface(MultiSurface),
+enum GeomStructSeparate {
+    Surface(Surface),
 }
 
-#[repr(u8)]
-enum Geometry_u8 {
-    Point(Point),
-    MultiPoint(MultiPoint),
-    Solid(Solid),
-    MultiSurface(MultiSurface),
+// Named fields in the variant so that we attach the data directly to the variant
+enum GeomStructEmbed {
+    Surface {
+        boundaries: Vec<LineString>,
+        semantics: Option<SemanticSurface>,
+        material: Option<Material>,
+        texture: Option<Texture>,
+    },
 }
 
-enum Geometry_p {
-    Point(Point),
+pub enum Geometry {
+    MultiPoint {
+        lod: String,
+        boundaries: Vec<Point>,
+    },
+    MultiLineString {
+        lod: String,
+        boundaries: Vec<LineString>,
+    },
+    MultiSurface {
+        lod: String,
+        boundaries: Vec<Surface>,
+    },
+    CompositeSurface {
+        lod: String,
+        boundaries: Vec<Surface>,
+    },
+    Solid {
+        lod: String,
+        boundaries: Vec<Shell>,
+    },
+    MultiSolid {
+        lod: String,
+        boundaries: Vec<crate::Geometry>, // This is not good here. I want to constrain this to a Solid.
+    },
+    CompositeSolid {
+        lod: String,
+        boundaries: Vec<crate::Geometry>,
+    },
 }
 
 struct CityObject {
@@ -56,48 +102,127 @@ fn main() {
     let mut new_geoms: Vec<Geometry> = Vec::new();
 
     println!(
-        "size of Point is {}",
+        "type size of a single Point is {}",
         std::mem::size_of_val(&[1.0, 2.0, 3.0])
     );
     println!(
-        "size of Geometry::Point is {}",
-        std::mem::size_of_val(&Geometry::Point([1.0, 2.0, 3.0]))
+        "value size of a single Point [1.0, 2.0, 3.0] is {}",
+        std::mem::size_of_val(&[1.0, 2.0, 3.0])
     );
+    let mp = Geometry::MultiPoint {
+        lod: "1.2".to_string(),
+        boundaries: vec![[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]],
+    };
     println!(
-        "size of Geometry_u8::Point is {}",
-        std::mem::size_of_val(&Geometry_u8::Point([1.0, 2.0, 3.0]))
+        "type size of Geometry::MultiPoint is {}",
+        std::mem::size_of_val(&mp)
     );
-    println!(
-        "size of Geometry_p::Point is {}",
-        std::mem::size_of_val(&Geometry_p::Point([1.0, 2.0, 3.0]))
-    );
-    let mut s: Solid = vec![vec![vec![vec![
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-    ]]]];
-    s.shrink_to_fit();
-    println!("size of Solid is {}", std::mem::size_of_val(&s));
-    println!(
-        "size of Geometry::Solid is {}",
-        std::mem::size_of_val(&Geometry::Solid(s))
-    );
+    match mp {
+        Geometry::MultiPoint { lod, boundaries } => {
+            println!("MultiPoint lod is {}", lod)
+        }
+        Geometry::MultiLineString { .. } => {}
+        Geometry::MultiSurface { .. } => {}
+        Geometry::CompositeSurface { .. } => {}
+        Geometry::Solid { .. } => {}
+        Geometry::MultiSolid { .. } => {}
+        Geometry::CompositeSolid { .. } => {}
+    }
 
-    let solid: Solid = vec![vec![vec![vec![
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-    ]]]];
-    let msrf: MultiSurface = vec![vec![vec![
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-    ]]];
-    new_geoms.push(Geometry::Solid(solid));
-    new_geoms.push(Geometry::MultiSurface(msrf));
+    let g = GeomStructSeparate::Surface(Surface {
+        boundaries: vec![vec![[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]],
+        semantics: None,
+        material: None,
+        texture: None,
+    });
+
+    // This seems to me a bit more ergonomic compared to GeomStructEmbed, because I don't
+    // need to list all the data members
+    if let GeomStructSeparate::Surface(gsimp) = g {
+        println!(
+            "is Surface struct and has boundaries {:?}",
+            gsimp.boundaries
+        )
+    }
+
+    let ge = GeomStructEmbed::Surface {
+        boundaries: vec![vec![[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]],
+        semantics: None,
+        material: Some(Material {
+            name: "someMaterial".to_string(),
+            ambient_intensity: None,
+            diffuse_color: None,
+            emissive_color: None,
+            specular_color: None,
+            shininess: None,
+            transparency: None,
+            is_smooth: None,
+        }),
+        texture: None,
+    };
+    // Compared to GeomStrucSeparate, here I have to know all the data members and list them
+    if let GeomStructEmbed::Surface {
+        boundaries,
+        semantics,
+        material,
+        texture,
+    } = &ge
+    {
+        println!("is Surface struct and has boundaries {:?}", boundaries)
+    }
+    // See https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html#ignoring-remaining-parts-of-a-value-with-
+    // But I think this is going to be fine with this partial matching
+    if let GeomStructEmbed::Surface { material, .. } = &ge {
+        {
+            println!(
+                "is Surface struct and has material {:?}",
+                material.as_ref().expect("there is no material").name
+            )
+        }
+    }
+
+    // Let's see how does it look for a complete CityModel
+    new_geoms.push(Geometry::Solid {
+        lod: "2.2".to_string(),
+        boundaries: vec![vec![
+            Surface {
+                boundaries: vec![vec![
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                ]],
+                semantics: Some(SemanticSurface::GroundSurface),
+                material: None,
+                texture: None,
+            },
+            Surface {
+                boundaries: vec![vec![
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                    [1.0, 2.0, 3.0],
+                ]],
+                semantics: Some(SemanticSurface::WallSurface),
+                material: None,
+                texture: None,
+            },
+        ]],
+    });
+    new_geoms.push(Geometry::MultiSurface {
+        lod: "1".to_string(),
+        boundaries: vec![Surface {
+            boundaries: vec![vec![
+                [1.0, 2.0, 3.0],
+                [1.0, 2.0, 3.0],
+                [1.0, 2.0, 3.0],
+                [1.0, 2.0, 3.0],
+            ]],
+            semantics: None,
+            material: None,
+            texture: None,
+        }],
+    });
 
     let new_co = CityObject {
         cotype: "Building".to_string(),
@@ -115,11 +240,12 @@ fn main() {
         for geom in co.geometry {
             println!("type of geom {}", std::any::type_name_of_val(&geom));
             match geom {
-                Geometry::Solid(solidgeom) => {
+                Geometry::Solid { boundaries, .. } => {
                     println!("This is a Solid");
-                    for shell in solidgeom {
+                    for shell in boundaries {
                         for surface in shell {
-                            for ring in surface {
+                            println!("This surface is a {:?}", surface.semantics.unwrap());
+                            for ring in surface.boundaries {
                                 for point in ring {
                                     println!("{:?}", point)
                                 }
@@ -127,10 +253,10 @@ fn main() {
                         }
                     }
                 }
-                Geometry::MultiSurface(msrfgeom) => {
+                Geometry::MultiSurface { boundaries, .. } => {
                     println!("This is a MultiSurface");
-                    for surface in msrfgeom {
-                        for ring in surface {
+                    for surface in boundaries {
+                        for ring in surface.boundaries {
                             for point in ring {
                                 println!("{:?}", point)
                             }
