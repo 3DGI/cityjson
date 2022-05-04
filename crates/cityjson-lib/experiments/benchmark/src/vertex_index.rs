40e4aec626a94ffa06558a601c10306c6a792429
Balázs Dukai <balazs.dukai@gmail.com>
2022-05-04 13:15:59 +0200

    Revert Stream CityObjects for vertex-index

--- crates/cityjson-lib/experiments/benchmark/src/vertex_index.rs
+++ crates/cityjson-lib/experiments/benchmark/src/vertex_index.rs
@@ -1,29 +1,26 @@
 //! Vertex-index architecture.
 use std::collections::HashMap;
-use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, Read};
-use std::marker::PhantomData;
 use std::path::{Path, PathBuf};
 
-use memmap2::{Mmap, MmapOptions};
-use serde::de::{EnumAccess, Error, MapAccess, SeqAccess, Visitor};
-use serde::{Deserialize, Deserializer};
+use memmap2::MmapOptions;
+use serde::Deserialize;
 
 // Deserialize into indexed CityJSON-like structures with serde
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct SemanticSurface {
     #[serde(rename = "type")]
     semtype: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct Semantics {
     surfaces: Vec<SemanticSurface>,
     values: Vec<Vec<usize>>,
 }
 
-type Vertices = Vec<[i32; 3]>;
+type Vertices = Vec<[f64; 3]>;
 
 // Indexed geometry
 type Vertex = usize;
@@ -33,7 +30,7 @@ type Shell = Vec<Surface>;
 type MultiSurface = Vec<Surface>;
 type Solid = Vec<Shell>;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 #[serde(tag = "type")]
 enum Geometry {
     MultiSurface {
@@ -48,20 +45,20 @@ enum Geometry {
     },
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct CityObject {
     #[serde(rename = "type")]
     cotype: String,
     geometry: Vec<Geometry>,
 }
 
-#[derive(Deserialize, Default, Debug)]
+#[derive(Deserialize)]
 struct Transform {
     scale: [f64; 3],
     translate: [f64; 3],
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct CityModel {
     #[serde(rename = "type")]
     cmtype: String,
@@ -72,96 +69,11 @@ struct CityModel {
     vertices: Vertices,
 }
 
-#[derive(Deserialize, Debug)]
-struct CMCObjects {
-    #[serde(rename = "type")]
-    #[serde(skip)]
-    cmtype: String,
-    #[serde(skip)]
-    version: String,
-    #[serde(skip)]
-    transform: Transform,
-    #[serde(rename = "CityObjects")]
-    cityobjects: HashMap<String, CityObject>,
-    #[serde(skip)]
-    vertices: Vertices,
-}
-
-#[derive(Deserialize, Debug)]
-struct CMVertices {
-    #[serde(rename = "type")]
-    cmtype: String,
-    version: String,
-    transform: Transform,
-    #[serde(skip)]
-    cityobjects: HashMap<String, CityObject>,
-    vertices: Vertices,
-}
-
-fn for_each<'de, D, K, V, F>(deserializer: D, f: F) -> Result<(), D::Error>
-where
-    D: Deserializer<'de>,
-    K: Deserialize<'de>,
-    V: Deserialize<'de>,
-    F: FnMut(K, V),
-{
-    struct MapVisitor<K, V, F>(F, PhantomData<K>, PhantomData<V>);
-
-    impl<'de, K, V, F> Visitor<'de> for MapVisitor<K, V, F>
-    where
-        K: Deserialize<'de>,
-        V: Deserialize<'de>,
-        F: FnMut(K, V),
-    {
-        type Value = ();
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("a nonempty sequence")
-        }
-
-        fn visit_map<A>(mut self, mut seq: A) -> Result<(), A::Error>
-        where
-            A: MapAccess<'de>,
-        {
-            while let Some((coid, value)) = seq.next_entry::<K, V>()? {
-                // println!("we are at {}", coid);
-                self.0(coid, value)
-            }
-            Ok(())
-        }
-    }
-    let visitor = MapVisitor(f, PhantomData, PhantomData);
-    deserializer.deserialize_map(visitor)
-}
-
-#[derive(Deserialize, Debug)]
-struct CMCObjectsTest {
-    #[serde(rename = "CityObjects")]
-    cityobjects: HashMap<String, CityObject>,
-}
-
 pub fn vindex_deserialize(path_in: PathBuf) {
-    let mut cm_vertices: CMVertices;
-    let mut cm: CMCObjects;
-
-    {
-        let path_in = "/home/balazs/Development/cjlib/experiments/data/dummy.json";
-        let mut file = File::open(&path_in).expect("Couldn't open CityJSON file");
-        let mmap = unsafe { Mmap::map(&file).expect("Cannot memmap the file") };
-        // let reader = BufReader::new(&file);
-        cm_vertices = serde_json::from_slice(&mmap).expect("Couldn't deserialize the Vertices");
-    }
-
-    let mut file = File::open(&path_in).expect("Couldn't open CityJSON file");
-    let reader = BufReader::new(&file);
-    // cm = serde_json::from_reader(reader).expect("Couldn't deserialize into CityModel");
-    let mmap = unsafe { Mmap::map(&file).expect("Cannot memmap the file") };
-    let mut deserializer = serde_json::Deserializer::from_slice(&mmap);
-    // let mut deserializer = serde_json::Deserializer::from_reader(reader);
-    for_each(&mut deserializer, |key: String, value: CityObject| {
-        println!("key {} value {:#?}", key, value);
-    })
-    .expect("error");
+    let mut file = File::open(path_in).expect("Couldn't open CityJSON file");
+    let reader = BufReader::new(file);
+    let cm: CityModel =
+        serde_json::from_reader(reader).expect("Couldn't deserialize into CityModel");
 }
 
 #[cfg(test)]
@@ -169,9 +81,7 @@ mod tests {
     use super::*;
 
     fn get_data() -> PathBuf {
-        // let p = "/home/balazs/Development/cjlib/experiments/data/3dbag_v210908_fd2cee53_5786_bench.city.json";
-        let p = "../data/simple.json";
-        Path::new(p)
+        Path::new("/home/balazs/Development/cjlib/experiments/data/3dbag_v210908_fd2cee53_5786_bench.city.json")
             .canonicalize()
             .expect("Could not find the INPUT file.")
     }
@@ -190,7 +100,7 @@ mod tests {
         file.read_to_end(&mut buffer)
             .expect("Couldn't read CityJSON file contents");
         // let reader = BufReader::new(file);
-        let cm: CMCObjects =
+        let cm: CityModel =
             serde_json::from_slice(&buffer[..]).expect("Couldn't deserialize into CityModel");
     }
 }
