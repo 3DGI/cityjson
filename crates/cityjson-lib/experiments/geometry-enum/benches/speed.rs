d4ff21d97a3afe0de5ae1eca0a1adde87ce2300a
Balázs Dukai <balazs.dukai@gmail.com>
2024-03-22 16:00:03 +0100

    Try Generic Boundary struct
    Get the RawValue of the geometry boundary and then deserialize that based on the geometry type

--- crates/cityjson-lib/experiments/geometry-enum/benches/speed.rs
+++ crates/cityjson-lib/experiments/geometry-enum/benches/speed.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
-use criterion::{black_box, Criterion, criterion_group, criterion_main, SamplingMode};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, SamplingMode};
 
 /// Deserialize into a serde_json::Value.
 fn serde_value<P: AsRef<Path>>(path: P) -> serde_json::Result<serde_json::Value> {
@@ -14,16 +14,6 @@ fn serde_value<P: AsRef<Path>>(path: P) -> serde_json::Result<serde_json::Value>
     Ok(val)
 }
 
-fn strongly_typed<P: AsRef<Path>>(
-    path: P,
-) -> serde_json::Result<Vec<nested::MultiSurfaceBoundary>> {
-    let mut file = File::open(path.as_ref()).unwrap();
-    let mut json_str = String::new();
-    file.read_to_string(&mut json_str).unwrap();
-    let ms: Vec<nested::MultiSurfaceBoundary> = serde_json::from_str(&json_str).unwrap();
-    Ok(ms)
-}
-
 /// Benchmark with real data. Run 'just download' first to download the data files.
 fn real_data(c: &mut Criterion) {
     let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
@@ -31,61 +21,15 @@ fn real_data(c: &mut Criterion) {
         .join("data");
     let sampling_mode = SamplingMode::Flat;
 
-    let mut group = c.benchmark_group("boundaries");
-    let p_json = data_dir.join("boundaries.json");
-    group.sampling_mode(sampling_mode);
-    group.bench_function("strongly_typed", |b| {
-        b.iter_with_large_drop(|| {
-            let ms = strongly_typed(black_box(&p_json)).unwrap();
-            black_box(ms);
-        })
-    });
-    group.bench_function("serde_json::Value", |b| {
-        b.iter_with_large_drop(|| {
-            let val = serde_value(black_box(&p_json)).unwrap();
-            black_box(val);
-        })
-    });
-    group.finish();
-
-    let mut group = c.benchmark_group("vertices");
-    let p_json = data_dir.join("vertices.json");
-    group.sampling_mode(sampling_mode);
-    group.bench_function("strongly_typed", |b| {
-        b.iter_with_large_drop(|| {
-            let mut file = File::open(&p_json).unwrap();
-            let mut json_str = String::new();
-            file.read_to_string(&mut json_str).unwrap();
-            let ms: Vec<[i64; 3]> = serde_json::from_str(&json_str).unwrap();
-            black_box(ms);
-        })
-    });
-    group.bench_function("serde_json::Value", |b| {
-        b.iter_with_large_drop(|| {
-            let val = serde_value(black_box(&p_json)).unwrap();
-            black_box(val);
-        })
-    });
-    group.finish();
-
     let mut group = c.benchmark_group("all");
     let p_json = data_dir.join("all.json");
     group.sampling_mode(sampling_mode);
-    group.bench_function("strongly_typed:GeometryStruct", |b| {
+    group.bench_function("strongly_typed", |b| {
         b.iter_with_large_drop(|| {
             let mut file = File::open(&p_json).unwrap();
             let mut json_str = String::new();
             file.read_to_string(&mut json_str).unwrap();
-            let ms: nested::ModelGeomStruct = serde_json::from_str(&json_str).unwrap();
-            black_box(ms);
-        })
-    });
-    group.bench_function("strongly_typed:GeometryEnum", |b| {
-        b.iter_with_large_drop(|| {
-            let mut file = File::open(&p_json).unwrap();
-            let mut json_str = String::new();
-            file.read_to_string(&mut json_str).unwrap();
-            let ms: nested::ModelGeomEnum = serde_json::from_str(&json_str).unwrap();
+            let ms: nested::Model = serde_json::from_str(&json_str).unwrap();
             black_box(ms);
         })
     });
--- crates/cityjson-lib/experiments/geometry-enum/src/lib.rs
+++ crates/cityjson-lib/experiments/geometry-enum/src/lib.rs
@@ -1,66 +1,149 @@
 #![allow(dead_code)]
+
 use std::collections::HashMap;
 use std::fmt;
 
-use serde::de::{DeserializeSeed, Deserializer, SeqAccess, Visitor};
-use serde::Deserialize;
+use serde::de::{
+    DeserializeSeed, Deserializer, IgnoredAny, IntoDeserializer, MapAccess, SeqAccess, Visitor,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::value::RawValue;
 
 #[derive(Debug, Default, Deserialize)]
-pub struct ModelGeomEnum {
-    cityobjects: HashMap<String, CityObjectGeomEnum>,
+pub struct Model {
+    cityobjects: HashMap<String, CityObject>,
     vertices: Vec<[i64; 3]>,
 }
 
 #[derive(Debug, Default, Deserialize)]
-pub struct ModelGeomStruct {
-    cityobjects: HashMap<String, CityObjectGeomStruct>,
-    vertices: Vec<[i64; 3]>,
+pub struct CityObject {
+    geometry: Vec<Geometry>,
 }
 
-#[derive(Debug, Default, Deserialize)]
-pub struct CityObjectGeomEnum {
-    geometry: Vec<GeometryEnum>
-}
-
-#[derive(Debug, Default, Deserialize)]
-pub struct CityObjectGeomStruct {
-    geometry: Vec<GeometryStruct>
+#[derive(Debug)]
+struct Geometry {
+    type_geom: GeometryType,
+    lod: String,
+    boundaries: Boundary,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(tag = "type")]
-pub enum GeometryEnum {
-    MultiSurface {
-        boundaries: MultiSurfaceBoundary
+enum GeometryType {
+    MultiPoint,
+    MultiLineString,
+    MultiSurface,
+    CompositeSurface,
+    Solid,
+    MultiSolid,
+    CompositeSolid,
+    GeometryInstance,
+}
+
+struct JsonRawValue<'a>(&'a RawValue);
+
+struct GeometryVisitor;
+
+impl<'de> Visitor<'de> for GeometryVisitor {
+    type Value = Geometry;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a valid Geometry object")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: MapAccess<'de>,
+    {
+        let mut geomtype_op: Option<GeometryType> = None;
+        let mut boundaries_rw = serde_json::value::to_raw_value(&Boundary::default()).unwrap();
+        let mut lod_rw = serde_json::value::to_raw_value(&String::new()).unwrap();
+
+        while let Some(key) = map.next_key::<String>()? {
+            if key == "type" {
+                geomtype_op = Some(map.next_value::<GeometryType>()?);
+            } else if key == "boundaries" {
+                boundaries_rw = map.next_value::<Box<RawValue>>()?;
+            } else if key == "lod" {
+                lod_rw = map.next_value::<Box<RawValue>>()?;
+            } else {
+                map.next_value::<IgnoredAny>()?;
+            }
+        }
+
+        if geomtype_op.is_none() {
+            return Err(serde::de::Error::custom(
+                "did not find the key 'type' in the Geometry",
+            ));
+        }
+        let geomtype = geomtype_op.unwrap();
+
+        let mut boundaries = Boundary::default();
+        match geomtype {
+            GeometryType::MultiPoint => {
+                todo!()
+            }
+            GeometryType::MultiLineString => {
+                todo!()
+            }
+            GeometryType::MultiSurface => {
+                boundaries_rw
+                    .deserialize_seq(ExtendSurfacesVisitor(&mut boundaries))
+                    .map_err(serde::de::Error::custom)?;
+            }
+            GeometryType::CompositeSurface => {
+                boundaries_rw
+                    .deserialize_seq(ExtendSurfacesVisitor(&mut boundaries))
+                    .map_err(serde::de::Error::custom)?;
+            }
+            GeometryType::Solid => {
+                boundaries_rw
+                    .deserialize_seq(ExtendShellsVisitor(&mut boundaries))
+                    .map_err(serde::de::Error::custom)?;
+            }
+            GeometryType::MultiSolid => {
+                todo!()
+            }
+            GeometryType::CompositeSolid => {
+                todo!()
+            }
+            GeometryType::GeometryInstance => {
+                todo!()
+            }
+        }
+        let lod =
+            String::deserialize(lod_rw.into_deserializer()).map_err(serde::de::Error::custom)?;
+
+        Ok(Geometry {
+            type_geom: geomtype,
+            lod,
+            boundaries,
+        })
     }
 }
 
-#[derive(Debug, Deserialize)]
-#[serde(tag = "type")]
-pub struct GeometryStruct {
-    #[serde(rename = "type")]
-    type_geom: String,
-    boundaries: MultiSurfaceBoundary
+struct GeometryTypeContainer(GeometryType);
+
+impl<'de> Deserialize<'de> for Geometry {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_map(GeometryVisitor)
+    }
 }
 
-#[derive(Debug, Default)]
-struct BoundaryContainer {
+#[derive(Debug, Default, Serialize)]
+struct Boundary {
     vertices: Vec<usize>,
     rings: Vec<usize>,
     surfaces: Vec<usize>,
+    shells: Vec<usize>,
+    solids: Vec<usize>,
 }
 
-#[derive(Clone, Debug, Default)]
-#[allow(dead_code)]
-pub struct MultiSurfaceBoundary {
-    vertices: Vec<usize>,
-    rings: Vec<usize>,
-    surfaces: Vec<usize>,
-}
+struct ExtendVertices<'a>(&'a mut Boundary);
 
-struct ExtendVertices<'a>(&'a mut BoundaryContainer);
-
-struct ExtendVerticesVisitor<'a>(&'a mut BoundaryContainer);
+struct ExtendVerticesVisitor<'a>(&'a mut Boundary);
 
 impl<'de, 'a> Visitor<'de> for ExtendVerticesVisitor<'a> {
     type Value = ();
@@ -96,9 +179,9 @@ impl<'de, 'a> DeserializeSeed<'de> for ExtendVertices<'a> {
     }
 }
 
-struct ExtendRings<'a>(&'a mut BoundaryContainer);
+struct ExtendRings<'a>(&'a mut Boundary);
 
-struct ExtendRingsVisitor<'a>(&'a mut BoundaryContainer);
+struct ExtendRingsVisitor<'a>(&'a mut Boundary);
 
 impl<'de, 'a> Visitor<'de> for ExtendRingsVisitor<'a> {
     type Value = ();
@@ -134,9 +217,9 @@ impl<'de, 'a> DeserializeSeed<'de> for ExtendRings<'a> {
     }
 }
 
-struct ExtendSurfaces<'a>(&'a mut BoundaryContainer);
+struct ExtendSurfaces<'a>(&'a mut Boundary);
 
-struct ExtendSurfacesVisitor<'a>(&'a mut BoundaryContainer);
+struct ExtendSurfacesVisitor<'a>(&'a mut Boundary);
 
 impl<'de, 'a> Visitor<'de> for ExtendSurfacesVisitor<'a> {
     type Value = ();
@@ -177,18 +260,166 @@ impl<'de, 'a> DeserializeSeed<'de> for ExtendSurfaces<'a> {
     }
 }
 
-impl<'de> Deserialize<'de> for MultiSurfaceBoundary {
+struct ExtendShells<'a>(&'a mut Boundary);
+
+struct ExtendShellsVisitor<'a>(&'a mut Boundary);
+
+impl<'de, 'a> Visitor<'de> for ExtendShellsVisitor<'a> {
+    type Value = ();
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a solid boundary array")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        // Add the start index of the first shell of the aggregate
+        self.0.shells.push(self.0.surfaces.len());
+        // Each iteration through this loop is one inner array.
+        while let Some(()) = seq.next_element_seed(ExtendSurfaces(self.0))? {
+            self.0.shells.push(self.0.surfaces.len());
+        }
+        if !self.0.shells.is_empty() {
+            let last_idx = self.0.shells.len() - 1;
+            self.0.shells.remove(last_idx);
+        }
+        Ok(())
+    }
+}
+
+impl<'de, 'a> DeserializeSeed<'de> for ExtendShells<'a> {
+    type Value = ();
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_seq(ExtendShellsVisitor(self.0))
+    }
+}
+
+impl<'de> Deserialize<'de> for Boundary {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let mut boundarycontainer = BoundaryContainer::default();
+        let mut boundary = Boundary::default();
         deserializer
-            .deserialize_seq(ExtendSurfacesVisitor(&mut boundarycontainer))
-            .map(|_| Self {
-                vertices: boundarycontainer.vertices,
-                rings: boundarycontainer.rings,
-                surfaces: boundarycontainer.surfaces,
-            })
+            .deserialize_seq(ExtendShellsVisitor(&mut boundary))
+            .map(|_| boundary)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs::File;
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn deser_all() {
+        let mut file = File::open("benches/data/all.json").unwrap();
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let ms: Model = serde_json::from_str(&json_str).unwrap();
+        println!("done")
+    }
+
+    #[test]
+    fn deserialize_solidboundary() {
+        let solidboundary_json = r#"[]"#;
+        let solidboundary: Boundary = serde_json::from_str(solidboundary_json).unwrap();
+        assert!(
+            solidboundary.shells.is_empty()
+                && solidboundary.surfaces.is_empty()
+                && solidboundary.rings.is_empty()
+                && solidboundary.vertices.is_empty()
+        );
+
+        let solidboundary_json =
+            r#"[[ [[0, 3, 2, 1]], [[4, 5, 6, 7]], [[0, 1, 5, 4]], [[1, 2, 6, 5]] ]]"#;
+        let solidboundary: Boundary = serde_json::from_str(solidboundary_json).unwrap();
+        assert_eq!(
+            solidboundary.vertices,
+            vec![0_usize, 3, 2, 1, 4, 5, 6, 7, 0, 1, 5, 4, 1, 2, 6, 5]
+        );
+        assert_eq!(solidboundary.rings, vec![0_usize, 4, 8, 12]);
+        assert_eq!(solidboundary.surfaces, vec![0_usize, 1, 2, 3]);
+        assert_eq!(solidboundary.shells, vec![0_usize]);
+
+        let solidboundary_json =
+            r#"[[ [[0, 3, 2, 1]], [[4, 5, 6, 7]], [[0, 1, 5, 4]], [[1, 2, 6, 5]] ], []]"#;
+        let solidboundary: Boundary = serde_json::from_str(solidboundary_json).unwrap();
+        assert_eq!(
+            solidboundary.vertices,
+            vec![0_usize, 3, 2, 1, 4, 5, 6, 7, 0, 1, 5, 4, 1, 2, 6, 5]
+        );
+        assert_eq!(solidboundary.rings, vec![0_usize, 4, 8, 12]);
+        assert_eq!(solidboundary.surfaces, vec![0_usize, 1, 2, 3]);
+        // Surface index 4 is out of bounds, which indicates and empty shell.
+        assert_eq!(solidboundary.shells, vec![0_usize, 4]);
+
+        let solidboundary_json = r#"[ [ [[0, 3, 2, 1], [4, 5, 6, 7]] ], [ [[0, 1, 5, 4]] ] ]"#;
+        let solidboundary: Boundary = serde_json::from_str(solidboundary_json).unwrap();
+        assert_eq!(
+            solidboundary.vertices,
+            vec![0_usize, 3, 2, 1, 4, 5, 6, 7, 0, 1, 5, 4]
+        );
+        assert_eq!(solidboundary.rings, vec![0_usize, 4, 8]);
+        assert_eq!(solidboundary.surfaces, vec![0_usize, 2]);
+        assert_eq!(solidboundary.shells, vec![0_usize, 1]);
+    }
+
+    #[test]
+    fn deserialize_aggregatesurfaceboundary() {
+        let multisurfaceboundary_json = r#"[]"#;
+        let msrfbdry: Boundary = serde_json::from_str(multisurfaceboundary_json).unwrap();
+        assert!(
+            msrfbdry.surfaces.is_empty()
+                && msrfbdry.rings.is_empty()
+                && msrfbdry.vertices.is_empty()
+        );
+
+        let multisurfaceboundary_json = r#"[[[0, 3, 2, 1]]]"#;
+        let msrfbdry: Boundary = serde_json::from_str(multisurfaceboundary_json).unwrap();
+        assert_eq!(msrfbdry.vertices, vec![0_usize, 3, 2, 1]);
+        assert_eq!(msrfbdry.rings, vec![0_usize]);
+
+        let multisurfaceboundary_json = r#"[ [[0, 3, 2, 1], [4, 5, 6, 7]], [[0, 3, 2, 1]] ]"#;
+        let msrfbdry: Boundary = serde_json::from_str(multisurfaceboundary_json).unwrap();
+        assert_eq!(
+            msrfbdry.vertices,
+            vec![0_usize, 3, 2, 1, 4, 5, 6, 7, 0, 3, 2, 1]
+        );
+        assert_eq!(msrfbdry.rings, vec![0_usize, 4, 8]);
+        assert_eq!(msrfbdry.surfaces, vec![0_usize, 2]);
+    }
+
+    #[test]
+    fn deserialize_surfaceboundary() {
+        let surfaceboundary_json = r#"[]"#;
+        let surfaceboundary: Boundary = serde_json::from_str(surfaceboundary_json).unwrap();
+        assert!(surfaceboundary.rings.is_empty() && surfaceboundary.vertices.is_empty());
+
+        let surfaceboundary_json = r#"[[0, 3, 2, 1]]"#;
+        let surfaceboundary: Boundary = serde_json::from_str(surfaceboundary_json).unwrap();
+        assert_eq!(surfaceboundary.vertices, vec![0_usize, 3, 2, 1]);
+        assert_eq!(surfaceboundary.rings, vec![0_usize]);
+
+        let surfaceboundary_json = r#"[[0, 3, 2, 1], [4, 5, 6, 7]]"#;
+        let surfaceboundary: Boundary = serde_json::from_str(surfaceboundary_json).unwrap();
+        assert_eq!(surfaceboundary.vertices, vec![0_usize, 3, 2, 1, 4, 5, 6, 7]);
+        assert_eq!(surfaceboundary.rings, vec![0_usize, 4]);
+
+        let surfaceboundary_json = r#"[[0, 3, 2, 1], [4, 5, 6, 7], [4, 5, 6, 7]]"#;
+        let surfaceboundary: Boundary = serde_json::from_str(surfaceboundary_json).unwrap();
+        assert_eq!(
+            surfaceboundary.vertices,
+            vec![0_usize, 3, 2, 1, 4, 5, 6, 7, 4, 5, 6, 7]
+        );
+        assert_eq!(surfaceboundary.rings, vec![0_usize, 4, 8]);
     }
 }
