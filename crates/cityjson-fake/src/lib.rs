2b012c58b47e7dc75f7ef434a44023611598eb77
Balázs Dukai <balazs.dukai@gmail.com>
2024-10-02 11:52:44 +0200

    Write invalid files to disk

--- crates/cityjson-fake/src/lib.rs
+++ crates/cityjson-fake/src/lib.rs
@@ -275,13 +275,13 @@ impl<'cm> CityModelBuilder<'cm> {
             }
             .fake(),
         );
-        self.extra = Some(
-            AttributesFaker {
-                random_values: false,
-                random_keys: false,
-            }
-            .fake(),
-        );
+        // self.extra = Some(
+        //     AttributesFaker {
+        //         random_values: false,
+        //         random_keys: false,
+        //     }
+        //     .fake(),
+        // );
         self
     }
 
@@ -2457,6 +2457,34 @@ impl Dummy<F64Faker> for f64 {
 mod tests {
     use super::*;
     use cjval::CJValidator;
+    use std::env;
+
+    fn invalids_dir() -> PathBuf {
+        std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("tests")
+            .join("invalids")
+    }
+
+    fn count_invalids(invalids_dir: &PathBuf) -> usize {
+        let mut count: usize = 0;
+        for entry in std::fs::read_dir(invalids_dir).unwrap() {
+            if entry.is_ok() {
+                let p = entry.unwrap().path();
+                if p.extension().is_some_and(|ext| ext == "json") {
+                    let name = p.file_name().unwrap();
+                    let d = name
+                        .to_string_lossy()
+                        .replace("cjfake_invalid_", "")
+                        .replace(".city.json", "");
+                    let c = d.parse::<usize>().unwrap();
+                    if c > count {
+                        count = c;
+                    }
+                }
+            }
+        }
+        count
+    }
 
     #[test]
     fn attributes() {
@@ -2510,6 +2538,20 @@ mod tests {
         println!("{}", &cj_str);
         let val = CJValidator::from_str(&cj_str);
         // assert!(val.validate().iter().all(|(c, s)| s.is_valid()));
+        let invalids: Vec<(String, String)> = val
+            .validate()
+            .into_iter()
+            .filter(|(criterion, summary)| summary.is_valid())
+            .map(|(criterion, summary)| (criterion, summary.to_string()))
+            .collect();
+        if invalids.len() > 0 {
+            // Serialize invalid citymodels for later analysis
+            let idir = invalids_dir();
+            let invalids_count = count_invalids(&idir);
+            let current_invalid_nr = invalids_count + 1;
+            let fname = format!("cjfake_invalid_{}.city.json", current_invalid_nr);
+            std::fs::write(idir.join(fname), cj_str).unwrap();
+        }
         for (criterion, summary) in val.validate().iter() {
             assert!(
                 summary.is_valid(),
