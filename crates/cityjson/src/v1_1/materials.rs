3d7853510d3780e072bacbae9aa93662f122cf06
Balázs Dukai <balazs.dukai@gmail.com>
2025-01-24 23:34:48 +0100

    Implement Material

--- crates/cityjson/src/v1_1/materials.rs
+++ crates/cityjson/src/v1_1/materials.rs
@@ -1,10 +1,10 @@
-use std::borrow::Cow;
+use crate::storage::{BorrowedStringStorage, OwnedStringStorage, StringStorage};
 
 pub type Rgb = [f32; 3];
 
-#[derive(Clone, Default, Debug, PartialEq)]
-pub struct Material<'cm> {
-    pub name: Cow<'cm, str>,
+#[derive(Clone, Debug, PartialEq)]
+pub struct Material<S: StringStorage> {
+    pub name: S::String,
     pub ambient_intensity: Option<f32>,
     pub diffuse_color: Option<Rgb>,
     pub emissive_color: Option<Rgb>,
@@ -12,4 +12,40 @@ pub struct Material<'cm> {
     pub shininess: Option<f32>,
     pub transparency: Option<f32>,
     pub is_smooth: Option<bool>,
+}
+
+impl<S: StringStorage> Material<S> {
+    pub fn new(name: S::String) -> Self {
+        Self {
+            name,
+            ambient_intensity: None,
+            diffuse_color: None,
+            emissive_color: None,
+            specular_color: None,
+            shininess: None,
+            transparency: None,
+            is_smooth: None,
+        }
+    }
+}
+
+pub type OwnedMaterial = Material<OwnedStringStorage>;
+pub type BorrowedMaterial<'a> = Material<BorrowedStringStorage<'a>>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_owned_material() {
+        let mat = OwnedMaterial::new("brick".to_string());
+        assert_eq!(mat.name, "brick");
+    }
+
+    #[test]
+    fn test_borrowed_material() {
+        let name = "brick";
+        let mat = BorrowedMaterial::new(name);
+        assert_eq!(mat.name, "brick");
+    }
 }
