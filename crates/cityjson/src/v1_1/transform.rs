0e2d568f95f489895b65b30856c31c9ad2f79703
Balázs Dukai <balazs.dukai@gmail.com>
2025-02-25 14:57:40 +0100

    Implement Transform

--- crates/cityjson/src/v1_1/transform.rs
+++ crates/cityjson/src/v1_1/transform.rs
@@ -1,6 +1,44 @@
 //! # Transform
 //!
 //! Represents a [Transform object](https://www.cityjson.org/specs/1.1.3/#transform-object).
-pub struct Transform;
 
-impl crate::cityjson::transform::Transform for Transform {}
+use std::fmt::{Display, Formatter};
+use crate::cityjson;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Transform {
+    pub scale: [f64; 3],
+    pub translate: [f64; 3],
+}
+
+impl Transform {
+    pub fn new() -> Self {
+        Self {
+            scale: [1.0, 1.0, 1.0],
+            translate: [0.0, 0.0, 0.0],
+        }
+    }
+}
+
+impl Display for Transform {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "(scale: [{}, {}, {}], translate:[{}, {}, {}])",
+            self.scale[0],
+            self.scale[1],
+            self.scale[2],
+            self.translate[0],
+            self.translate[1],
+            self.translate[2]
+        )
+    }
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl cityjson::transform::Transform for Transform {}
