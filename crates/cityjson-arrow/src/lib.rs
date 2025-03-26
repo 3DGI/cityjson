729e2a2e40fbdc5caca8fcf796afb8928525d7f5
Balázs Dukai <balazs.dukai@gmail.com>
2025-03-26 06:31:03 +0100

    Translate

--- crates/cityjson-arrow/src/lib.rs
+++ crates/cityjson-arrow/src/lib.rs
@@ -1,26 +1,32 @@
-use std::error::Error;
 use arrow::array::{
-    ArrayRef, FixedSizeListBuilder, Float64Builder, Int64Builder,
-    RecordBatch, StructArray,
+    ArrayRef, FixedSizeListBuilder, Float64Builder, Int64Builder, RecordBatch, StructArray,
 };
 use arrow::datatypes::{DataType, Field, Schema};
 use cityjson::prelude::{QuantizedCoordinate, TransformTrait};
 use cityjson::v2_0::Transform;
+use std::error::Error;
 use std::sync::Arc;
 
+// todo: create specific error and conversion for crate
 pub fn transform_to_arrow(transform: &Transform) -> Result<RecordBatch, Box<dyn Error>> {
     let scale_builder = Float64Builder::new();
     let translate_builder = Float64Builder::new();
     // Wrap the f64 arrays in FixedSizeListArrays.
-    let mut scale_array_builder = FixedSizeListBuilder::with_capacity(scale_builder, 3, 1);
-    let mut translate_array_builder = FixedSizeListBuilder::with_capacity(translate_builder, 3, 1);
+    let mut scale_array_builder = FixedSizeListBuilder::with_capacity(scale_builder, 3, 1)
+        .with_field(Field::new("item", DataType::Float64, false));
+    let mut translate_array_builder = FixedSizeListBuilder::with_capacity(translate_builder, 3, 1)
+        .with_field(Field::new("item", DataType::Float64, false));
     // Build the Lists
-    scale_array_builder.values().extend(transform.scale().into_iter().map(|v| Some(v)));
+    transform
+        .scale()
+        .into_iter()
+        .for_each(|v| scale_array_builder.values().append_value(v));
     scale_array_builder.append(true);
     let scale_list = scale_array_builder.finish();
-    translate_array_builder
-        .values()
-        .extend(transform.translate().into_iter().map(|v| Some(v)));
+    transform
+        .translate()
+        .into_iter()
+        .for_each(|v| translate_array_builder.values().append_value(v));
     translate_array_builder.append(true);
     let translate_list = translate_array_builder.finish();
 
@@ -50,7 +56,7 @@ pub fn transform_to_arrow(transform: &Transform) -> Result<RecordBatch, Box<dyn 
 
 #[test]
 fn test_transform() {
-        // Create a Transform with known values
+    // Create a Transform with known values
     let mut transform = Transform::new();
     transform.set_scale([0.1, 0.2, 0.3]);
     transform.set_translate([10.0, 20.0, 30.0]);
@@ -63,7 +69,6 @@ fn test_transform() {
     assert_eq!(batch.num_columns(), 2);
     assert_eq!(batch.schema().field(0).name(), "scale");
     assert_eq!(batch.schema().field(1).name(), "translate");
-
 }
 
 #[derive(Debug, Default)]
